7289d237b21e3d7bcb61631034d8953e25f89403
Jon Rutter <contact@jonrutter.com>
2021-09-02 15:42:14 -0400

    Added plugins to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,22 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Huddle Landing Page",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Huddle Landing Page',
   },
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-root-import',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#FF52BF`,
+        display: `standalone`,
+      },
+    },
+  ],
 };
