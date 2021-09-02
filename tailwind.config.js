8ec6ffd3313656bf73b0c6fc10dac4aa8fe43a9c
Jon Rutter <contact@jonrutter.com>
2021-09-02 17:25:00 -0400

    Extend base Tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#FF52BF',
+        light: '#EBFBFF',
+        grey: '#808D99',
+        dark: '#00252E',
+      },
+      backgroundImage: {
+        'hero-desktop': `url('images/bg-hero-desktop.svg')`,
+        'hero-mobile': `url('images/bg-hero-mobile.svg')`,
+      },
+      maxWidth: {
+        200: '200px',
+        124: '124px',
+      },
+      fontFamily: {
+        sans: ['"Open Sans"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        heading: [
+          'Poppins',
+          '"Open Sans"',
+          'ui-sans-serif',
+          'system-ui',
+          'sans-serif',
+        ],
+      },
+    },
   },
   variants: {
     extend: {},
