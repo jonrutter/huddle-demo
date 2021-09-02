3829a5cd6b3d96934a4b8760f6c13fa0a8a65369
Jon Rutter <contact@jonrutter.com>
2021-09-02 17:25:40 -0400

    Added Header layout component

---
+++ src/components/layout/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// logo
+import Logo from 'images/logo.svg';
+
+// components
+import WhiteButton from 'components/buttons/WhiteButton';
+
+const Header = () => {
+  return (
+    <header className="flex justify-between flex-wrap items-center mb-16 gap-4">
+      <img src={Logo} alt="Huddle" className="max-w-124 md:max-w-200 h-auto" />
+      <WhiteButton>Try It Free</WhiteButton>
+    </header>
+  );
+};
+
+export default Header;
