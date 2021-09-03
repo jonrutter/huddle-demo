5bb17529de6b968dca4096245281e4303b631ae5
Jon Rutter <contact@jonrutter.com>
2021-09-03 14:53:26 -0400

    GitHub preview

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: 'https://www.yourdomain.tld',
+    siteUrl: 'https://jonrutter-huddle-landing-page.netlify.app/',
     title: 'Huddle Landing Page',
   },
   plugins: [
